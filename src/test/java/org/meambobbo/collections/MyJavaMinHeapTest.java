db93caf69997577b6f9230c2120b7d998b0ace0b
meambobbo <meambobbo@foobazaar.com>
2020-05-26 15:40:28 -0500

    compare my min heap to priorityqueue

--- src/test/java/org/meambobbo/collections/MyJavaMinHeapTest.java
+++ src/test/java/org/meambobbo/collections/MyJavaMinHeapTest.java
@@ -3,6 +3,8 @@ package org.meambobbo.collections;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.PriorityQueue;
+
 public class MyJavaMinHeapTest {
 
     @Test
@@ -37,5 +39,40 @@ public class MyJavaMinHeapTest {
         System.out.println(heap.toString());
         Assert.assertEquals(heap.pop(), Integer.valueOf(500));
         System.out.println(heap.toString());
+
+        System.out.println();
+        System.out.println("Compare to Priority Queue");
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(20);
+        System.out.println(priorityQueue.toString());
+        priorityQueue.add(1);
+        System.out.println(priorityQueue.toString());
+        priorityQueue.add(1);
+        System.out.println(priorityQueue.toString());
+        priorityQueue.add(-100);
+        System.out.println(priorityQueue.toString());
+        priorityQueue.add(500);
+        System.out.println(priorityQueue.toString());
+        priorityQueue.add(1);
+        System.out.println(priorityQueue.toString());
+        priorityQueue.add(300);
+        System.out.println(priorityQueue.toString());
+
+        Assert.assertEquals(priorityQueue.poll(), Integer.valueOf(-100));
+        System.out.println(priorityQueue.toString());
+        Assert.assertEquals(priorityQueue.poll(), Integer.valueOf(1));
+        System.out.println(priorityQueue.toString());
+        Assert.assertEquals(priorityQueue.poll(), Integer.valueOf(1));
+        System.out.println(priorityQueue.toString());
+        Assert.assertEquals(priorityQueue.poll(), Integer.valueOf(1));
+        System.out.println(priorityQueue.toString());
+        Assert.assertEquals(priorityQueue.poll(), Integer.valueOf(20));
+        System.out.println(priorityQueue.toString());
+        Assert.assertEquals(priorityQueue.poll(), Integer.valueOf(300));
+        System.out.println(priorityQueue.toString());
+        Assert.assertEquals(priorityQueue.poll(), Integer.valueOf(500));
+        System.out.println(priorityQueue.toString());
+        
     }
 }
